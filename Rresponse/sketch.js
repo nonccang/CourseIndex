709048d0ef7902194d63ec5c9339b0de0aa1cb5b
nonccang <99106694+nonccang@users.noreply.github.com>
2022-03-18 23:39:22 -0400

    Add files via upload

---
+++ Rresponse/sketch.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".R1A").css({ display: "none" });
+
+  $(".R1").click(function () {
+    $(".R1A").slideToggle();
+  });
+});
+
+$(document).ready(function () {
+  $(".R2A").css({ display: "none" });
+
+  $(".R2").click(function () {
+    $(".R2A").slideToggle();
+  });
+});
+
+$(document).ready(function () {
+  $(".R3A").css({ display: "none" });
+
+  $(".R3").click(function () {
+    $(".R3A").slideToggle();
+  });
+});
